09a48fdb7ee25878dbf6aee3acdc557aa49ea806
dkudo <dkudo@ncsu.edu>
2019-03-31 23:56:09 -0400

    Implemented and tested SimpleArrayList class.

--- Project2/src/edu/ncsu/csc216/travel/list_utils/SimpleArrayList.java
+++ Project2/src/edu/ncsu/csc216/travel/list_utils/SimpleArrayList.java
@@ -12,10 +12,10 @@ package edu.ncsu.csc216.travel.list_utils;
 public class SimpleArrayList<E> implements SimpleList<E> {
 
 	/** size of this array which is used for both the initial size and also for the size extension. */
-	static private int RESIZE = 12;
+	private static final int RESIZE = 12;
 	/** list used for holding elements of this array. */
-	private Object[] list;
-	/** current size of this array. */
+	private E[] list;
+	/** current number of elements in this array. */
 	private int size;
 	
 
@@ -23,62 +23,118 @@ public class SimpleArrayList<E> implements SimpleList<E> {
 	 * Constructs SimpleArrayList.
 	 */
 	public SimpleArrayList() {
-		// TODO Auto-generated constructor stub
+		this(RESIZE);
 	}
 	
 	/**
-	 * Constructs SimpleArrayList of given size.
-	 * @param size : size of the array
+	 * Constructs SimpleArrayList of a given size.
+	 * @param initialSize : internal maximum size for initialization
 	 */
-	public SimpleArrayList(int size) {
-		// TODO Auto-generated constructor stub
+	@SuppressWarnings("unchecked")
+	public SimpleArrayList(int initialSize) {
+		this.list = (E[]) new Object[initialSize];
+		this.size = 0;
 	}
 	
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.size;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		if (this.size == 0) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean contains(E e) {
-		// TODO Auto-generated method stub
+		for (int i = 0 ; i < this.size ; i++) {
+			if (this.list[i].equals(e)) {
+				return true;
+			}
+		}
 		return false;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public boolean add(E e) {
-		// TODO Auto-generated method stub
-		return false;
+		this.add(this.size(), e);
+		return true;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void add(int pos, E e) {
-		// TODO Auto-generated method stub
+		// @throws NullPointerException if the specified element is null and this
+	    //             list does not permit null elements
+	    // @throws IllegalArgumentException if some property of the specified
+	    //             element prevents it from being added to this list
+	    // @throws IndexOutOfBoundsException if the index is out of range 
+	    //             (pos < 0 || pos > size())
 		
+		if (e == null) {
+			throw new NullPointerException();
+		}
+		
+		if (pos < 0 || pos > size()) {
+			throw new IndexOutOfBoundsException();
+		}		
+		
+		// if the current array is full of elements
+		if (this.list.length == this.size()) {
+			E[] currentList = this.list;
+			this.list = (E[]) new Object[this.list.length + RESIZE];
+			
+			for (int i = 0 ; i < currentList.length ; i++) {
+				this.list[i] =  currentList[i];
+			}
+		}
+		if (0 < this.size) {
+			for (int i = this.size - 1 ; pos <= i ; i--) {
+				this.list[i+1] = this.list[i];
+			}
+		}
+		this.list[pos] = e;
+		this.size++;
 	}
 
 	@Override
-	public E remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+	public E remove(int pos) {
+		// @throws IndexOutOfBoundsException if the index is out of range (pos < 0
+	    //             || pos >= size())
+		
+		if (pos < 0 || pos >= this.size()){
+			throw new IndexOutOfBoundsException();
+		}
+		
+		E toBeRemoved = this.list[pos];
+		for (int i = pos ; i < this.size ; i++) {
+			this.list[i] = list[i + 1];
+		}
+		
+		this.list[this.size - 1] = null;
+		this.size--;
+		return toBeRemoved;
 	}
 
 	@Override
 	public E get(int pos) {
-		// TODO Auto-generated method stub
-		return null;
+		//  @throws IndexOutOfBoundsException if the index is out of range (pos < 0
+	    //             || pos >= size())
+		if (pos < 0 || pos >= size()) {
+			throw new IndexOutOfBoundsException();
+		}
+		return this.list[pos];
 	}
 
 	@Override
 	public int indexOf(E e) {
-		// TODO Auto-generated method stub
+		// @return the index of the first occurrence of the specified element in
+	    //         this list, or -1 if this list does not contain the element
 		return 0;
 	}
 
