9981ef9f67e9ebda43c912afcad76f09e81bd6a5
dkudo <dkudo@ncsu.edu>
2019-04-08 15:06:46 -0400

    Updated testing files for LandTour and River Cruise classes

--- Project2/test/edu/ncsu/csc216/travel/model/vacation/RiverCruiseTest.java
+++ Project2/test/edu/ncsu/csc216/travel/model/vacation/RiverCruiseTest.java
@@ -5,37 +5,376 @@ package edu.ncsu.csc216.travel.model.vacation;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+
 import org.junit.Test;
 
+import edu.ncsu.csc216.travel.model.participants.Client;
+
 /**
- * Test class for RiverCruise class
+ * Test class for EducationTrip class.
  * @author dkudo
  *
  */
 public class RiverCruiseTest {
 
 	/**
-	 * Test method for {@link edu.ncsu.csc216.travel.model.vacation.RiverCruise#getName()}.
+	 * Test method for {@link edu.ncsu.csc216.travel.model.vacation.Tour#Tour(java.lang.String, java.time.LocalDate, int, int, int)}.
 	 */
 	@Test
-	public void testGetName() {
-		fail("Not yet implemented");
-	}
+	public void testRiverCruise() {
+		Tour t = null;
+		try {
+			t = new RiverCruise("name", LocalDate.of(2019, 11, 11), 7, 500, 50);
+		} catch (IllegalArgumentException e) {
+			fail();			
+		}
+		assertEquals("RC-name", t.getName());
+		assertEquals(LocalDate.of(2019, 11, 11), t.getStartDate());
+		assertEquals(LocalDate.of(2019, 11, 18), t.getEndDate());
+		assertEquals(7, t.getDuration());
+		assertEquals(500, t.getBasePrice());
+		assertEquals(50, t.getCapacity());
+		assertEquals(7, t.getDuration());
+		
+	
+		
+		// invalid  : name = null;
+		try {
+			t = new RiverCruise(null, LocalDate.of(2019, 11, 11) , 7, 500, 50);
+			fail();
+		} catch (IllegalArgumentException e) {	
+			assertEquals(e.getMessage(), "Tour name is invalid.");
+		}
+		
+		// invalid  : name = empty 
+		try {
+			t = new RiverCruise("", LocalDate.of(2019, 11, 11), 7, 500, 50);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals(e.getMessage(), "Tour name is invalid.");
+		}
+		
+		// invalid : date == null
+		try {
+			t = new RiverCruise("name", null , 7, 500, 50);
+			fail();
+		} catch (IllegalArgumentException e) {	
+			assertEquals(e.getMessage(), "Tour start date is invalid.");
+		}
+		
+		// invalid : duration is negative or 0
+		try {
+			t = new RiverCruise("name", LocalDate.of(2019, 11, 11), -1, 500, 50);
+			fail();
+		} catch (IllegalArgumentException e) {	
+			assertEquals(e.getMessage(), "Tour duration is invalid.");
+		}
+		
+		// invalid : duration is negative or 0
+		try {
+			t = new RiverCruise("name", LocalDate.of(2019, 11, 11), 7, -1, 50);
+			fail();
+		} catch (IllegalArgumentException e) {	
+			assertEquals(e.getMessage(), "Tour base price is invalid.");
+		}
+				
+		// invalid : duration is negative or 0
+		try {
+			t = new RiverCruise("name", LocalDate.of(2019, 11, 11), 7, 500, -1);
+			fail();
+		} catch (IllegalArgumentException e) {	
+			assertEquals(e.getMessage(), "Tour capacity is invalid.");
+		}
+		
+}
 
+	
+	
 	/**
 	 * Test method for {@link edu.ncsu.csc216.travel.model.vacation.RiverCruise#costFor(int)}.
 	 */
 	@Test
 	public void testCostFor() {
-		fail("Not yet implemented");
+		 RiverCruise et = new RiverCruise("name", LocalDate.of(2019, 11, 11), 7, 500, 50);
+		 assertEquals(750, Math.round(et.costFor(1)));
+		 assertEquals(1000, (int)et.costFor(2));
+		 assertEquals(1750, (int)et.costFor(3));
+	}
+
+	
+	/**
+	 * Test method for {@link edu.ncsu.csc216.travel.model.vacation.Tour#compareTo(edu.ncsu.csc216.travel.model.vacation.Tour)}.
+	 */
+	@Test
+	public void testCompareTo() {
+		RiverCruise t1 = new RiverCruise("name", LocalDate.of(2019, 11, 11), 7, 500, 50);
+		
+		// name alphabetically earlier
+		RiverCruise t2 = new RiverCruise("a", LocalDate.of(2019, 11, 11), 7, 500, 50);
+		// name alphabetically earlier
+		RiverCruise t3 = new RiverCruise("z", LocalDate.of(2019, 11, 11), 7, 500, 50);
+		
+		assertEquals(1, t1.compareTo(t2));
+		assertEquals(-1, t1.compareTo(t3));
+		
+		
+		// same name but start date earlier
+		RiverCruise t4 = new RiverCruise("Name", LocalDate.of(2019, 10, 11), 7, 500, 50);
+		// same name but start date later
+		RiverCruise t5 = new RiverCruise("Name", LocalDate.of(2019, 12, 11), 7, 500, 50);
+		
+		assertEquals(1, t1.compareTo(t4));
+		assertEquals(-1, t1.compareTo(t5));
+		
+		// same name, same start date, but duration shorter
+		RiverCruise t6 = new RiverCruise("Name", LocalDate.of(2019, 11, 11), 6, 500, 50);
+		// same name, same start date, but duration longer
+		RiverCruise t7 = new RiverCruise("Name", LocalDate.of(2019, 11, 11), 8, 500, 50);
+		
+		assertEquals(1, t1.compareTo(t6));
+		assertEquals(-1, t1.compareTo(t7));
+		
+		// same name, same start date, same duration
+		RiverCruise t8 = new RiverCruise("Name", LocalDate.of(2019, 11, 11), 7, 500, 50);
+		
+		assertEquals(0, t1.compareTo(t8));
+		
+	
+		
 	}
 
 	/**
-	 * Test method for {@link edu.ncsu.csc216.travel.model.vacation.RiverCruise#RiverCruise(java.lang.String, java.time.LocalDate, int, int, int)}.
+	 * Test method for {@link edu.ncsu.csc216.travel.model.vacation.Tour#setCapacity(int)}.
 	 */
 	@Test
-	public void testRiverCruise() {
-		fail("Not yet implemented");
+	public void testSetCapacity() {
+		RiverCruise et = new RiverCruise("name", LocalDate.of(2019, 11, 11), 7, 500, 40);
+	
+		
+		// try to set capacity less than or equal to 0
+		try {
+			et.setCapacity(39);
+			fail();
+		} catch (CapacityException e) {
+			assertEquals(40, et.getCapacity());
+		}
+		
+		// double the capacity.
+		try {
+			et.setCapacity(80);
+		} catch (CapacityException e) {
+			fail();
+		}
+		assertEquals(80, et.getCapacity());
+		assertTrue(et.isCapacityFixed());
+		
+	}
+	
+	/**
+	 * Test method for createReservationFor()
+	 */
+	@Test
+	public void testNumberOfClientReservations() {
+
+		RiverCruise t1 = new RiverCruise("name1", LocalDate.of(2019, 10, 11), 7, 500, 50);
+		
+		try {
+			t1.createReservationFor(null, 10);
+			fail();
+		} catch(IllegalArgumentException e) {
+			assertEquals(0, t1.numberOfClientReservations());
+		} catch(CapacityException e) {
+			fail();
+		}
+			
+		try {
+			t1.createReservationFor(new Client("name1", "contact1"), 0);
+			fail();
+		} catch(IllegalArgumentException e) {
+			assertEquals(0, t1.numberOfClientReservations());
+		} catch(CapacityException e) {
+			fail();
+		}	
+		
+
+		try {
+			t1.createReservationFor(new Client("name1", "contact1"), 10);
+		} catch(IllegalArgumentException e) {
+			fail();
+		} catch(CapacityException e) {
+			fail();
+		}
+		assertEquals(1, t1.numberOfClientReservations());
+	}
+
+
+	/**
+	 * Test method for numberOfClientReservaiton(), spacesLeft(), getAllData()
+	 */
+	@Test
+	public void testReservations() {
+		RiverCruise t1 = new RiverCruise("name1", LocalDate.of(2019, 10, 11), 7, 500, 50);
+		
+		try {
+			t1.createReservationFor(new Client("name1", "contact1"), 10);
+			t1.createReservationFor(new Client("name2", "contact2"), 20);
+		} catch (CapacityException e) {
+			System.out.println(t1.numberOfClientReservations());
+			fail(e.getMessage());
+		}
+		assertEquals(2, t1.numberOfClientReservations());
+		assertEquals(20, t1.spacesLeft());
+		
+		// get the first reservation
+		assertEquals("name1", t1.getReservation(0).getClient().getName());
+		assertEquals("contact1", t1.getReservation(0).getClient().getContact());
+		assertEquals(10, t1.getReservation(0).getNumInParty());
+		
+		// get the second reservation
+		assertEquals("name1", t1.getReservation(0).getClient().getName());
+		assertEquals("contact1", t1.getReservation(0).getClient().getContact());
+		assertEquals(10, t1.getReservation(0).getNumInParty());
+		
+		
+		// get all data in this tour.
+		Object[] allData = t1.getAllData();
+		
+		// get the first reservation
+		assertEquals("name1", ((Reservation)allData[0]).getClient().getName());
+		assertEquals("contact1", ((Reservation)allData[0]).getClient().getContact());
+		assertEquals(10, ((Reservation)allData[0]).getNumInParty());
+		
+		// get the second reservation
+		assertEquals("name2", ((Reservation)allData[1]).getClient().getName());
+		assertEquals("contact2", ((Reservation)allData[1]).getClient().getContact());
+		assertEquals(20, ((Reservation)allData[1]).getNumInParty());
+		
+	}
+
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.travel.model.vacation.Tour#summaryInfo()}.
+	 */
+	@Test
+	public void testSummaryInfo() {
+		RiverCruise t1 = new RiverCruise("name1", LocalDate.of(2009, 9, 9), 7, 500, 50);
+		try {
+			t1.createReservationFor(new Client("name1", "contact1"), 10);
+			t1.createReservationFor(new Client("name2", "contact2"), 20);
+		} catch (Exception e) {
+			fail();
+		}
+		
+		assertEquals("RC-name1: 09/09/09 7 days", t1.summaryInfo());
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.travel.model.vacation.Tour#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEqualsObject() {
+		RiverCruise t1 = new RiverCruise("name1", LocalDate.of(2019, 10, 11), 7, 500, 50);
+		// equal
+		RiverCruise t2 = new RiverCruise("name1", LocalDate.of(2019, 10, 11), 7, 300, 40);
+		// different name
+		RiverCruise t3 = new RiverCruise("name3", LocalDate.of(2019, 10, 11), 7, 500, 50);
+		// different start date
+		RiverCruise t4 = new RiverCruise("name1", LocalDate.of(2019, 10, 12), 7, 500, 50);
+		// different duration
+		RiverCruise t5 = new RiverCruise("name1", LocalDate.of(2019, 10, 11), 3, 500, 50);
+		
+		
+		assertTrue(t1.equals(t2));
+		assertFalse(t1.equals(t3));
+		assertFalse(t1.equals(t4));
+		assertFalse(t1.equals(t5));
+	}
+
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.travel.model.vacation.Tour#listOfReservations()}.
+	 */
+	@Test
+	public void testListOfReservations() {
+		RiverCruise t1 = new RiverCruise("name1", LocalDate.of(2019, 10, 11), 7, 500, 50);
+		
+		Client c1 = new Client("name1", "contact1");
+		Client c2 = new Client("name2", "contact2");
+		Client c3 = new Client("name3", "contact3");
+		
+		try {
+			t1.createReservationFor(c1, 10);
+			t1.createReservationFor(c2, 20);
+		} catch (Exception e) {
+			fail();
+		}
+		
+		String[] list = t1.listOfReservations();
+		assertEquals(c1.summaryInfo(), list[0]);
+		assertEquals(c2.summaryInfo(), list[1]);
+	}
+
+
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.travel.model.vacation.Tour#addOldReservation(edu.ncsu.csc216.travel.model.vacation.Reservation)}.
+	 */
+	@Test
+	public void testAddOldReservation() {
+		Tour t1 = new RiverCruise("name1", LocalDate.of(2019, 10, 11), 7, 500, 50);
+		Reservation r1 = new Reservation(t1, new Client("name1", "contact1"), 10);
+		Reservation r2 = new Reservation(t1, new Client("name1", "contact1"), 20);
+		Reservation r3 = new Reservation(t1, new Client("name1", "contact1"), 30);
+		
+		try {
+			t1.addOldReservation(null);
+		} catch (IllegalArgumentException e) {
+			//pass
+		} catch (CapacityException e) {
+			fail();
+		}
+		
+		try {
+			t1.addOldReservation(r1);
+			t1.addOldReservation(r2);
+		} catch (CapacityException e) {
+			fail();
+		}
+		
+		try {
+			t1.addOldReservation(r3);
+			fail();
+		} catch (CapacityException e) {
+			assertEquals(2, t1.listOfReservations().length);
+		}
+		
+		
+	}
+	
+	/**
+	 * Test method for cancselReservation() and spaaceLeft()
+	 */
+	@Test
+	public void testCancelReservation() {
+		RiverCruise t1 = new RiverCruise("name1", LocalDate.of(2019, 10, 11), 7, 500, 50);
+		
+		Reservation r1 = null;
+		Reservation r2 = null;
+		Reservation r3 = null;
+		try {
+			r1 = t1.createReservationFor(new Client("name1", "contact1"), 10);
+			assertEquals(40, t1.spacesLeft());
+			r2 = t1.createReservationFor(new Client("name2", "contact2"), 20);
+			assertEquals(20, t1.spacesLeft());
+		} catch (Exception e) {
+			fail();
+		}
+		assertEquals(2, t1.numberOfClientReservations());
+		
+		t1.cancelReservation(r1);
+		assertEquals(r2, t1.getReservation(0));
+		
 	}
 
 }
