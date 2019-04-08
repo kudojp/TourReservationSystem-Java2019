b0fa861bb097092ad66a4f864785bcde2b02bfa2
dkudo <dkudo@ncsu.edu>
2019-04-08 15:58:43 -0400

    Same as before

--- Project2/src/edu/ncsu/csc216/travel/model/vacation/EducationalTrip.java
+++ Project2/src/edu/ncsu/csc216/travel/model/vacation/EducationalTrip.java
@@ -33,6 +33,27 @@ public class EducationalTrip extends Tour {
 	
 	
 	
+	/* (non-Javadoc)
+	 * @see edu.ncsu.csc216.travel.model.vacation.Tour#createReservationFor(edu.ncsu.csc216.travel.model.participants.Client, int)
+	 */
+	/**
+	@Override
+	public Reservation createReservationFor(Client c, int i) throws CapacityException {
+		
+		try {
+			return super.createReservationFor(c, i);
+		} catch(CapacityException cp) {
+			// double the capacity if it makes capable to accomodate
+			if (!super.isCapacityFixed() || super.spacesLeft() + super.getCapacity() >= i) {
+				super.setCapacity(super.getCapacity() * 2);
+				super.fixCapacity();
+				return super.createReservationFor(c, i);
+			}
+		}
+		// in the case when there is no space
+		throw new CapacityException();
+	}
+	*/
 
 
 	/* (non-Javadoc)
